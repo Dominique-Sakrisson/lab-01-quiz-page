f43f4270bf7fe9e1e6df98d0bdeb12f940891cb1
Dominique Sakrisson <domsakrisson@gmailcom>
2021-01-11 13:57:04 -0800

    add dav branch init. commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // import functions and grab DOM elements
+import {countsAsAYes} from './utils.js';
 
 // initialize state
-
+const testStart = document.getElementById('test-button');
+console.log(testStart);
 // set event listeners to update state and DOM
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,1 @@
-import './example.test.js';
+import {countsAsAYes} from '../utils.js';
